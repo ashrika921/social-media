320a18f8540d44356ea29e52b6f2ad89b050a7b8
ashrika <kadianashrika@gmail.com>
2020-06-06 21:10:34 +0530

    server check

--- test/controllers/users.test.js
+++ test/controllers/users.test.js
@@ -22,14 +22,13 @@ describe('controllers/users', () => {
     expect(foundUser.username).to.equal(createdUser.username)
   })
 
-  /* it('should through error for non number userid', async () => {
+   it('should through error for non number userid', async () => {
 
     await expect(getUserById('sss')).to.be.rejectedWith('user id should be integer')
     await expect(getUserById(null)).to.be.rejectedWith('user id not provided')
     await expect(getUserById(true)).to.be.rejectedWith('user id should be integer')
     await expect(getUserById(0)).to.be.rejectedWith('user id not provided')
-    
-  })    */
+  })
 
   it('should find user by username', async () => {
 
