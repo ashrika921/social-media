dfcfd2374b0c61eb8fa3b5ae458295d8685ddbfc
ashrika <kadianashrika@gmail.com>
2020-06-11 19:53:15 +0530

    updating PORT

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -32,26 +32,25 @@ module.exports = {
 }
 
 /* Test Code */
-/*
-async function task() {
-  // console.log(
-  //   await createNewPost(
-  //     1,
-  //     'This is a sample post',
-  //     'Body of the post goes here'
-  //   )
-  // ),
-  // console.log(
-  //   await createNewPost(
-  //     2,
-  //     'Another sample post',
-  //     'Some body example here as well'
-  //   )
-  // )
+
+/*   async function task() {
+   console.log(
+     await createNewPost(
+       1,
+       'This is a sample post',
+      'Body of the post goes here'
+     )
+   ),
+   console.log(
+     await createNewPost(
+       2,
+       'Another sample post',
+       'Some body example here as well'
+     )
+   )
   const posts = await showAllPosts()
   for (let p of posts) {
     console.log(`${p.title}\nauthor: ${p.user.username}\n${p.body}\n==========\n`)
   }
 }
-task()
-*/
+task()    */
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const { db } = require('./db/models')
 const { usersRoute } = require('./routes/users')
 const { postsRoute } = require('./routes/posts')
+const PORT = process.env.PORT||8383
 
 const app = express()
 app.use(express.json())
@@ -12,10 +13,10 @@ app.use('/api/users', usersRoute)
 app.use('/api/posts', postsRoute)
 app.use('/', express.static(__dirname + '/public'))
 
-db.sync()
+db.sync({alter:true})
   .then(() => {
-    app.listen(8383, () => {
-      console.log('server started on http://localhost:8383')
+    app.listen(PORT, () => {
+      console.log('server started on http://localhost:${PORT}')
     })
   })
   .catch((err) => {
