9d54924a76dcf4244d10b61b1ab02347b1d95965
ashrika <kadianashrika@gmail.com>
2020-10-04 22:07:42 +0530

    added comments

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,40 +1,26 @@
-const { Users } = require('../db/models')
-const { genRandomUsername } = require('../utils/username')
+  
+const { Users } = require("../db/models");
+const { genRandomUsername } = require("../utils/username");
 
 async function createAnonUser() {
-  const user = await Users.create({
+  return await Users.create({
     username: genRandomUsername(),
-  })
-
-  return user
+  });
 }
+
 async function getUserById(id) {
-   if (!id) throw new Error('user id not provided')
- if (typeof id !== 'number') throw new Error('user id should be integer') 
+  if (!id) throw new Error("user id not provided");
+  if (typeof id !== "number") throw new Error("user id should be integer");
 
-  return await Users.findOne({ where: { id } })
+  return await Users.findOne({ where: { id } });
 }
+
 async function getUserByUsername(username) {
-  return await Users.findOne({ where: { username } })
+  return await Users.findOne({ where: { username } });
 }
 
 module.exports = {
   createAnonUser,
   getUserById,
   getUserByUsername,
-}
-
-/*  Test Code */
-/*
-async function task () {
-    console.log(await createAnonUser())
-    console.log('---------------------')
-    console.log(await createAnonUser())
-    console.log('---------------------')
-    console.log(await createAnonUser())
-    console.log('---------------------')
-    console.log(await createAnonUser())
-    console.log('---------------------')
-}
-task()
-*/
+};
